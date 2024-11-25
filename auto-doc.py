84adcb83ff600153279d88044188c1aa5e0c0916
unknown <Liya.An@PNC.LAN>
2024-11-25 09:17:54 +0700

    code for certificate

--- auto-doc.py
+++ auto-doc.py
@@ -1,22 +1,3 @@
-import openpyxl
-from docxtpl import DocxTemplate
-
-def generate_student_docs(excel_file, doc_template):
-    workbook = openpyxl.load_workbook(excel_file)
-    sheet = workbook.active 
-    student_data = list(sheet.values)[1:]  
-    student_info = DocxTemplate(doc_template)
-    for student in student_data:
-        student_info.render({
-            "student_data": student[0] 
-        })      
-        student_name = student[0]  
-        my_file = f"{student_name}.docx"
-        student_info.save(my_file)
-    print("Documents generated successfully.")
-generate_student_docs("students_data.xlsx", "Doc1.docx")
-
-
 #Generation Certificate---------------------------------
 
 import pandas as pd
@@ -68,5 +49,3 @@ for index, row in data.iterrows():
 
     # Print confirmation
     print(f"Certificate generated for {name} and saved to {output_path}")
-
-print("All certificates have been generated!")
